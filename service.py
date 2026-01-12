e1f61180ff2bc1de323f2a18cd6a101a296cca88
eBvel <belyakov041098@gmail.com>
2026-01-12 15:15:13 +0700

    Removed sessions

--- service.py
+++ service.py
@@ -1,48 +1,40 @@
-from requests import Session
+import requests
 from data import GoogleMapEndPoints
 
 
 class GoogleMapAPI:
     def __init__(self):
-        self.__end_points = GoogleMapEndPoints
-        self.session = Session()
+        self.__end_points = GoogleMapEndPoints()
 
     def create_location(self, body):
         url = self.__end_points.url_of_create_location
         print(f"POST URL: {url}")
-        with self.session as session:
-            return session.post(url, body)
+        return requests.post(url, body)
 
     def get_location(self, place_id):
         url = self.__end_points.url_of_get_location + f"&place_id={place_id}"
         print(f"GET URL: {url}")
-        with self.session as session:
-            return session.get(url)
+        return requests.get(url)
 
     def get_multiple_locations(self, list_of_place_id):
-        with self.session as session:
-            for id in list_of_place_id:
-                url = self.__end_points.url_of_get_location \
-                      + f"&place_id={id}"
-                print(f"GET URL: {url}")
-                yield session.get(url)
+        for id in list_of_place_id:
+            url = self.__end_points.url_of_get_location + f"&place_id={id}"
+            print(f"GET URL: {url}")
+            yield requests.get(url)
 
     def create_multiple_locations(self, body, count_iterations):
         url = self.__end_points.url_of_create_location
-        with self.session as session:
-            for i in range(count_iterations):
-                print(f"POST URL: {url}")
-                yield session.post(url, body)
+        for i in range(count_iterations):
+            print(f"POST URL: {url}")
+            yield requests.post(url, body)
 
     def delete_location(self, body):
         url = self.__end_points.url_of_delete_location
         print(f"DELETE URL: {url}")
-        with self.session as session:
-            return session.delete(url, json=body)
+        return requests.delete(url, json=body)
 
     def delete_multiple_location(self, list_of_body):
         url = self.__end_points.url_of_delete_location
         print(f"DELETE URL: {url}")
-        with self.session as session:
-            for body in range(list_of_body):
-                yield session.delete(url, json=body)
+        for body in range(list_of_body):
+            yield requests.delete(url, json=body)
